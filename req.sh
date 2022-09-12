b9a41411b2fe481c3848efa6b91ba793d566c186
reza <reza138398123@gmail.com>
2022-09-12 10:41:10 +0430

    first commit

---
+++ req.sh
@@ -0,0 +1,7 @@
+pkg update && pkg upgrade -y
+pkg install wget vim nodejs fzf figlet vim-python vim-runtime
+wget https://github.com/wolandark/wim/releases/download/v2.1/wim-2.1.vimrc
+wget https://github.com/wolandark/wim/releases/download/v2.1/Hack.Regular.Nerd.Font.Complete.ttf
+git clone https://github.com/adi1090x/termux-style
+cd termux-style
+./install
